7ea23f33ecc9b1fb9eb6f6cd3e97bd8a8d4631cb
Anirudh Ramprasad <anirudh.ramp@gmail.com>
2023-06-03 21:53:19 -0700

    Nothing really productive

--- packages/next/src/pages/_app.tsx
+++ packages/next/src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "@/Theme";
 import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+
+  return <ChakraProvider><Component {...pageProps} /> </ChakraProvider>;
 };
 
 export default MyApp;
